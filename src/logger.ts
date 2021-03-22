879d7dcb69760b8c2c9d95a16510bb5205dd4886
Amit Dhamu <dev@amitd.co>
2021-03-22 22:53:03 +0000

    Minor logging update

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,18 @@
+/* eslint-disable no-console */
 export default (
   message: string,
   type: 'INFO' | 'WARNING' | 'ERROR' = 'INFO'
-): void =>
-  // eslint-disable-next-line no-console
-  console.log(`${new Date().toISOString()} --- ${type}: ${message}`)
+): void => {
+  const output = `${new Date().toISOString()} --- ${type}: ${message}`
+
+  switch (type) {
+    case 'WARNING':
+      console.warn(output)
+      break
+    case 'ERROR':
+      console.error(output)
+      break
+    default:
+      console.log(output)
+  }
+}
